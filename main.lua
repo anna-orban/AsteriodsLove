a41e5e8d23a321ae9a5dadd7b3da094b0d26fa43
Anna Orban <anna.orban@yahoo.com>
2018-09-06 22:47:16 +0100

    initial set up

---
+++ main.lua
@@ -0,0 +1,6 @@
+local screenWidth, screenHeight = love.graphics.getDimensions()
+
+function love.draw()
+    love.graphics.setColor(0, 0, 1)
+    love.graphics.circle('fill', 800 / 2, 600 / 2, 30)
+end
